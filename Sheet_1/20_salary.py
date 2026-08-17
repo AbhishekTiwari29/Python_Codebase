1ee7f1a0c85df0ed5d9cee91c62366d1d92484ae
Abhishek <abhishektiwarii2027@gmail.com>
2026-08-18 00:16:01 +0530

    update

--- Sheet_1/20_salary.py
+++ Sheet_1/20_salary.py
@@ -3,13 +3,18 @@ hra = (salary*10)/100
 da = (salary*5)/100
 pf = (salary*3)/100
 
-inHand_salary = salary - hra - da - pf 
-
 if  0 <= salary <= 100000 :
     print("K")
+    tax =0
 elif 1100000 <= salary <= 2000000:
-    print(inHand_salary - (inHand_salary*20)/100)
+    tax = salary*20/100
 elif 500000 <= salary <= 1000000 :
-    print(inHand_salary - (inHand_salary*10)/100)
+    tax = salary*10/100
+elif salary > 2000000:
+    tax = salary*30/100
 else:
-    print(inHand_salary - (inHand_salary*30)/100)
+    tax = 0
+
+inHand_salary = salary - hra - da - pf - tax
+
+print(inHand_salary)
