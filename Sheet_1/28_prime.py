641ba5427214710cb77815ff4030314f0fbb9014
Abhishek <abhishektiwarii2027@gmail.com>
2026-08-26 00:59:44 +0530

    update

--- Sheet_1/28_prime.py
+++ Sheet_1/28_prime.py
@@ -1,2 +1,11 @@
 num = int(input("Enter Number: "))
 
+if num < 2:
+    print("Not Prime")
+else:
+    for i in range(2 , num):
+        if num % i  == 0:
+            print("not prime")
+            break
+    else:
+            print(" prime")
